ec2edb644a3d3484fcfb2b59e732fa3f8b49e498
Jishnu Prakash K P <Jishnu@iazi.ch>
2021-05-09 11:50:12 +0530

    changes added

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # reverse_image_search
 Find similar images from your local machine using deep learning
 
+Use "env.txt" to build the environment. (find run command inside)
+
 I have used Pytorch resnet18 pre-trained model for extracting features from image.
 
 Similarity is found using cosine similarity between query image and images from our pc (imagedb)
 
+- Query Image
 ![](https://github.com/Jishnuprakash/reverse_image_search/blob/main/query.png)
 
+- Results
 ![](https://github.com/Jishnuprakash/reverse_image_search/blob/main/results.png)
 
+Please run revere_image_search.py
--- reverse_image_search.py
+++ reverse_image_search.py
@@ -1,14 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 17 14:22:24 2019
-
-@author: deeplearning
-"""
+#Imports
 import os
 import pandas as pd
 import imgOps as img
 
-# Prepare Database
+#### Prepare Database ####
 imageFolder = 'images'
 imageId = os.listdir(imageFolder)
 imagePath = [os.path.join(imageFolder,x) for x  in os.listdir(imageFolder)]
@@ -18,15 +13,19 @@ data = pd.DataFrame(list(zip(imageId,imagePath)), columns=['imageId','imagePath'
 # feature extraction (This might take 1 to 2 sec per image or less :) )
 data['feature'] = data.imagePath.apply(img.img2vec)
 
-# Reverse Image search
+#### Reverse Image search ####
 queryPath = "images/cup1.jpg"
 query = img.img2vec(queryPath)
 
+# Find cosine similarity
 data['match'] = data.feature.apply(lambda x: img.cos_sim(query,x))
 
+# Sort values
 data = data.sort_values(by=['match'], ascending=False).reset_index(drop=True)
 
+# Select top results
 results = data.head(6)
+
 # Show Results
 img.show_images([queryPath], ['Query Image'])
 img.show_images(images=results['imagePath'], titles=results['match'].astype(str)+" %", cols=3)
